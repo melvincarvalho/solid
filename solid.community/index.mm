9576a8cae650621f9abf3c251e45512e61ab6d53
Melvin Carvalho <melvincarvalho@gmail.com>
2020-03-26 23:39:26 +0100

    remove renew cert from task list

--- solid.community/index.mm
+++ solid.community/index.mm
@@ -1,4 +1,4 @@
-<map version="freeplane 1.7.0">
+<map version="freeplane 1.8.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="solid.community" FOLDED="false" ID="ID_1488379025" CREATED="1583605769442" MODIFIED="1583742051863" LINK="../index.mm" STYLE="oval">
 <font SIZE="18"/>
@@ -89,9 +89,6 @@
 <node TEXT="todo" POSITION="left" ID="ID_1636972351" CREATED="1583701871745" MODIFIED="1583742137390" LINK="https://gitlab.com/solid.community/proposals/-/issues">
 <edge COLOR="#ff0000"/>
 <node TEXT="ToS" ID="ID_1065183033" CREATED="1583742165749" MODIFIED="1583742181004" LINK="https://gitlab.com/solid.community/proposals/-/issues/1"/>
-<node TEXT="renew certificate" ID="ID_1501639836" CREATED="1584046505913" MODIFIED="1584046510068">
-<node TEXT="expires 23 march" ID="ID_831221203" CREATED="1584310561705" MODIFIED="1584310566906"/>
-</node>
 </node>
 </node>
 </map>
